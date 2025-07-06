6b3a44247b3f4a14491af8443d90c0d12121e605
cacauisadog <cbcaue@protonmail.com>
2025-07-06 13:17:36 -0300

    feat(css): install and setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Economec</h1>
+      <h1 className="text-2xl text-green-200">Economec</h1>
       <button onClick={() => setCount(prev => prev + 1)}>Clicked {count} times</button>
     </>
   )
