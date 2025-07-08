d745db071bba86f8d23943c312718c86c0e92a03
cacauisadog <cbcaue@protonmail.com>
2025-07-08 16:50:45 -0300

    chore: lint and format everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { Button } from '@/components/ui/button'
-import { useState } from 'react'
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1 className="text-2xl text-green-200">Economec</h1>
-      <Button onClick={() => setCount(prev => prev + 1)}>Clicked {count} times</Button>
+      <Button onClick={() => setCount((prev) => prev + 1)}>
+        Clicked {count} times
+      </Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
