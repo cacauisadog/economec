86208f168fd043ef5c19a76d1ca6c96b342372c6
cacauisadog <cbcaue@protonmail.com>
2025-10-03 19:12:44 -0300

    BREAKING: overhaul everything with shadcn "dashboard" block
    https://ui.shadcn.com/blocks#dashboard-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import { useState } from "react";
-import AddNewTransactionDialog from "@/components/add-new-transaction-dialog";
-import { Button } from "@/components/ui/button";
+import { AppSidebar } from "@/components/app-sidebar";
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { DataTable } from "@/components/data-table";
+import { SectionCards } from "@/components/section-cards";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-function App() {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
+import data from "./data/data.json";
 
-  function openAddNewTransactionDialog() {
-    setIsDialogOpen(true);
-  }
-
+export default function App() {
   return (
-    <>
-      <h1 className="text-2xl text-green-200">Economec</h1>
-      <Button onClick={openAddNewTransactionDialog}>
-        Adicionar nova transação
-      </Button>
-      <AddNewTransactionDialog
-        open={isDialogOpen}
-        onOpenChange={setIsDialogOpen}
-      />
-    </>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div>
+              <DataTable data={data} />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
-
-export default App;
